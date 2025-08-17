cb5f729c26647b83aba0efc9415110c7d8eaa754
Sam Baguley <hello@sambaguley.com>
2025-08-17 11:53:33 +0100

    Add asteroids

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
@@ -7,13 +10,27 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0    
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
---
+++ test.md
@@ -0,0 +1,20 @@
+# TEST
+
+What is *LUA*.
+This does have _formatting_. 
+**asdfas** bold text.
+
+## Heading 2
+
+asdfasdfasdf asd fasdf asdf
+
+
+Learn x in y minutes.
+
+help lua guide.
+
+https://learnxinyminutes.com/
+
+
+
+
